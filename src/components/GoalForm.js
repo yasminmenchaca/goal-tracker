6c3e2f3a75132278282cb31151361071296cb466
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-10 10:13:19 -0600

    goals submitting to airtable

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Tags from './Tags';
 
-export default function GoalForm({ courseAdded }) {
-    const [name, setName] = useState('');
-    const [link, setLink] = useState('');
+export default function GoalForm({goalAdded}) {
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
     const [tags, setTags] = useState([]);
     const [count, setCount] = useState(0);
 
     const resetForm = () => {
-        setName('');
-        setLink('');
+        setTitle('');
+        setText('');
         setCount(count + 1);
     };
 
     const submitGoal = async (e) => {
         e.preventDefault();
-        //TODO: Create the course
-        resetForm();
-        courseAdded();
+        try {
+            await fetch('/api/goals', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title,
+                    text,
+                    tags,
+                }),
+            });
+            resetForm();
+            goalAdded();
+        } catch (err) {
+            console.error(err);
+        }
+        console.log(title, text);
     };
 
     return (
@@ -26,29 +38,30 @@ export default function GoalForm({ courseAdded }) {
             <div className="card-body">
                 <form className="" onSubmit={submitGoal}>
                     <div className="form-group">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="title">title</label>
                         <input
                             type="text"
-                            name="name"
-                            value={name}
+                            name="title"
+                            value={title}
                             className="form-control"
-                            onChange={(e) => setName(e.target.value)}
+                            onChange={(e) => setTitle(e.target.value)}
                         />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="link">Link</label>
+                        <label htmlFor="text">text</label>
                         <input
                             type="text"
-                            name="link"
-                            value={link}
+                            name="text"
+                            value={text}
                             className="form-control"
-                            onChange={(e) => setLink(e.target.value)}
+                            onChange={(e) => setText(e.target.value)}
                         />
                     </div>
                     <div className="form-group">
                         <p>Tags</p>
-                        <Tags tagsUpdated={setTags} key={count} />
+                        <Tags tagsUpdated={setTags} key={count}/>
                     </div>
+
                     <button type="submit" className="btn btn-primary">
                         Submit
                     </button>
