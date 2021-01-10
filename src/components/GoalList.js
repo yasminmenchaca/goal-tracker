43298ef483d0ae58d27f2351300744dcfe649ccd
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-01-09 22:03:25 -0600

    goals

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import Course from './Course';
+import Goal from './Goal';
 
-export default function CourseList({ courses, refreshCourses }) {
+export default function GoalList({ goals, refreshGoals }) {
     return (
         <div>
             <h2 className="mt-5 mb-3">Backlog</h2>
             <div className="list-group">
-                {courses
-                    .filter((course) => !course.purchased)
-                    .map((course) => (
-                        <Course
-                            course={course}
-                            key={course.id}
-                            refreshCourses={refreshCourses}
+                {goals
+                    .filter((goal) => !goal.completed)
+                    .map((goal) => (
+                        <Goal
+                            goal={goal}
+                            key={goal.id}
+                            refreshGoals={refreshGoals}
                         />
                     ))}
             </div>
             <h2 className="mt-5 mb-3">Already Purchased</h2>
-            {courses
-                .filter((course) => course.purchased)
-                .map((course) => (
-                    <Course
-                        course={course}
-                        key={course.id}
-                        refreshCourses={refreshCourses}
+            {goals
+                .filter((goal) => goal.completed)
+                .map((goal) => (
+                    <Goal
+                        goal={goal}
+                        key={goal.id}
+                        refreshGoals={refreshGoals}
                     />
                 ))}
         </div>
